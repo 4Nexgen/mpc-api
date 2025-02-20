2c93507c72532cccc6770a51cdddc90d3c44f684
Flourence Lapore <renzo.lapore@gmail.com>
2025-02-20 09:42:59 +0800

    Optimized Code

--- src/Controllers/votes/dto/create-vote.dto.ts
+++ src/Controllers/votes/dto/create-vote.dto.ts
@@ -1,9 +1,5 @@
-import { IsEmail, IsArray, ArrayNotEmpty, IsInt } from 'class-validator';
 export class CreateVoteDto {
   id: number;
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsInt({ each: true })
   candidate_ids: number[];
   email: string;
   remarks: string;
