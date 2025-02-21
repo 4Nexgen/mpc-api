46c6f3bc5509c3061ca2ac32b3a0f35eaf7e853d
Flourence Lapore <renzo.lapore@gmail.com>
2025-02-21 14:43:10 +0800

    Configured .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,22 +6,26 @@ import { Candidate } from './Controllers/candidates/entities/candidate.entity';
 import { Vote } from './Controllers/votes/entities/vote.entity';
 import { CandidatesModule } from './Controllers/candidates/candidates.module';
 import { VotesModule } from './Controllers/votes/votes.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }), // Load environment variables globally
     VotesModule,
     CandidatesModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      // password: 'jks@admin',
-      database: 'mpcsurveydb',
-      entities: [Candidate, Vote],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Candidate, Vote],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [AppController],
